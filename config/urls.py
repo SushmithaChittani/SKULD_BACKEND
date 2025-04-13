ba8a7ff2a71b92221f3bb6aaa8ad31ed0e122bb4
SushmithaChittani <sushmitha.c20@gmail.com>
2025-04-13 19:22:48 -0400

    urls fix2

--- config/urls.py
+++ config/urls.py
@@ -38,11 +38,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fitness_app.urls')),
-    path('api/', include('fitness_app.urls')),
-    path('api/', include('fitness_app.urls')),
-    path('api/', include('fitness_app.urls')),
-    path('api/', include('fitness_app.urls')),
-    path('api/', include('fitness_app.urls')),
     
     # Documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
